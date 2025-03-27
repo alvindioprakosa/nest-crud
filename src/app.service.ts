62cd87af1a278ec9c02ad1ad6c20c4111af9b2c7
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-27 08:20:35 +0700

    Update Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!!';
+  constructor(private readonly configService: ConfigService) {}
+
+  async getHello(): Promise<string> {
+    return this.configService.get<string>('HELLO_MESSAGE') || 'Hello World!!';
   }
 }
