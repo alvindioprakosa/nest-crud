ac62ef312a5ac9d08ce25e87431c7954c35f4f52
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-15 11:11:10 +0700

    Update task Service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
+
+// Mock implementation
+const mockTaskRepository = () => ({
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  remove: jest.fn(),
+});
 
 describe('TasksService', () => {
   let service: TasksService;
+  let repository: jest.Mocked<Repository<Task>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(Task),
+          useFactory: mockTaskRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
+    repository = module.get(getRepositoryToken(Task));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all tasks', async () => {
+    const mockTasks = [{ id: 1, text: 'Test', day: 'Today', reminder: false }];
+    repository.find.mockResolvedValue(mockTasks as any);
+    
+    const result = await service.findAll();
+    expect(result).toEqual(mockTasks);
+    expect(repository.find).toHaveBeenCalled();
   });
 });
